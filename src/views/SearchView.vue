2929709482716fd61953586ba607c23b255438a6
Ingrid <ingrid.lepik@gmail.com>
2023-12-07 16:41:53 +0200

    SearchView and DynamicUsersList added

---
+++ src/views/SearchView.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="container text-center">
+    <div class="row justify-content-center">
+      <div class="col col-4">
+        <h1>Tere!</h1>
+      </div>
+    </div>
+
+    <div class="row justify-content-center">
+      <div class="col col-4">
+      <div>
+        <div>
+          <input type="text" class="form-control" placeholder="Search for users">
+        </div>
+      </div>
+      </div>
+
+
+      <div class="row justify-content-center">
+        <div class="col col-2">
+          <button type="button" class="btn btn-outline-secondary m-3">Search</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script setup>
+
+//
+//export default {
+//  name: 'SearchView',
+//
+//  data() {
+//    return {
+//      headerTitle: 'Search for users',
+//    }
+//  },
+//
+//  methods: {
+//
+//  }
+//
+//
+//}
+
+</script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
